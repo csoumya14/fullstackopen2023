96fb7ba42e151e958e9f8f966b8c9b4a437b88de
Soumya Chalakkal <soumyachalakkal@gmail.com>
2023-12-16 12:57:55 +0100

    added anecdote assignment

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const DisplayedAnecdote = ({ anecdoteSelected, voteValueOfSelected }) => {
+  return (
+    <>
+      <h1>Anecdote of the Day</h1>
+      <p>{anecdoteSelected}</p>
+      <p>has {voteValueOfSelected} votes</p>
+    </>
+  );
+};
+
+const HighestVotedAnecdote = ({ anecdote, highestVoteValue }) => {
+  return (
+    <>
+      <h2>Anecdote with most votes</h2>
+      <p>
+        {anecdote} has {highestVoteValue}
+        votes
+      </p>
+    </>
+  );
+};
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+  const [selected, setSelected] = useState(0);
+  const [voteValue, setVoteValue] = useState(Array(anecdotes.length).fill(0));
+  const randomNumberGenerator = (max) => {
+    return Math.floor(Math.random() * max);
+  };
+
+  const handleClick = () => {
+    setSelected(randomNumberGenerator(anecdotes.length));
+  };
+
+  const addVotesOnclick = () => {
+    const newVoteArray = [...voteValue];
+    newVoteArray[selected] += 1;
+    setVoteValue(newVoteArray);
+  };
+
+  const indexOflargestValue = voteValue.indexOf(Math.max(...voteValue));
+
+  return (
+    <div>
+      <DisplayedAnecdote
+        anecdoteSelected={anecdotes[selected]}
+        voteValueOfSelected={voteValue[selected]}
+      />
+
+      <Button handleClick={handleClick} text="next anecdote" />
+      <Button handleClick={addVotesOnclick} text="votes" />
+      <HighestVotedAnecdote
+        anecdote={anecdotes[indexOflargestValue]}
+        highestVoteValue={voteValue[indexOflargestValue]}
+      />
+    </div>
+  );
+};
+
+export default App;
