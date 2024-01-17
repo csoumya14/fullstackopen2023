ed0d9e04c530998fa4baac86e6eace29a9dd10a9
Soumya Chalakkal <soumyachalakkal@gmail.com>
2024-01-17 14:47:51 +0100

    added cors

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
+
+app.use(cors());
+
 app.use(express.json());
 const morgan = require("morgan");
 app.use(morgan("tiny"));
@@ -96,7 +100,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
